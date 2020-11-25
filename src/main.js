848bd3fba479d14e5e2a16ff7bddd96c99006a9c
Megumi Miyatani <megumi.miyatani@gmail.com>
2020-11-26 07:19:08 +0900

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import LoadScript from 'vue-plugin-load-script'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(LoadScript)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+
+        <b-container>
+
+            <b-row>
+
+                <b-col v-for="(product, productIndex) in products" :key="productIndex">
+
+                    <h3>{{ product.title }}</h3>
+
+                    <b-btn @click="onAddToCartClick(product)">Add To Cart</b-btn>
+
+                </b-col>
+
+            </b-row>
+
+            <b-row>
+
+                <b-col>
+
+                    <b-btn @click="onCheckoutClick">Checkout</b-btn>
+
+                </b-col>
+
+            </b-row>
+
+        </b-container>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+let stripe;
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
+    name: 'Home',
+
+    mounted() {
+        this.loadStripe().then(() => {
+            stripe = new Stripe(process.env.VUE_APP_STRIPE_PUBLIC_KEY);
+        });
+    },
+
+    beforeDestroy() {
+        this.unloadStripe();
+    },
+
+    data: function () {
+        return {
+            products: [
+                {
+                    title: 'Orange Shirt'
+                },
+                {
+                    title: 'Blue Shirt'
+                }
+            ],
+            lineItems: []
+        }
+    },
+
+    methods: {
+
+        loadStripe () {
+            return this.$loadScript('https://js.stripe.com/v3');
+        },
+
+        unloadStripe () {
+            return this.$unloadScript('https://js.stripe.com/v3');
+        },
+
+        onAddToCartClick (product) {
+            this.lineItems.push({
+                title: product.title,
+                quantity: 1
+            })
+        },
+
+        onCheckoutClick () {
+            axios.post('http://stripe-test-project.megumidev/api/v1/stripe/create-checkout-session', {
+                line_items: this.lineItems
+            })
+                .then((response) => {
+                    return stripe.redirectToCheckout({ sessionId: response.data.data.id });
+                })
+                .then((result) => {
+                    if (result.error) {
+                        alert(result.error.message);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+
+    }
+
 }
 </script>
